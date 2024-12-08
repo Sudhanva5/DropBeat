a9f52eaf6154014b98fc92d61ae163e9979174f9
Sudhanva Acharya <Sudhanva@Sudhanvas-MacBook-Pro-5.local>
2024-12-08 13:43:38 +0530

    Initial project setup: Basic macOS app structure

---
+++ DropBeat/DropBeat/DropBeat/DropBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropBeatApp.swift
+//  DropBeat
+//
+//  Created by Sudhanva Acharya on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DropBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
